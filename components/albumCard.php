57a36fcfe7a67afd86755e88738d57c78f2a9708
Bravehearts19 <leo18111998@gmail.com>
2021-12-01 16:13:23 +0100

    Milestone 1 achieved

--- components/albumCard.php
+++ components/albumCard.php
@@ -1,16 +1,18 @@
-<div class="card">
-    <img class="card-img-top" src="<?php echo $album['poster']; ?>" alt="" />
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $album["title"]; ?></h5>
-        <div class="card-text">
-            <ul>
-                <li>
-                    <span class="author_span"><?php echo $album["author"]; ?></span>
-                </li>
-                <li>
-                    <span class="year_span"><?php echo $album["year"]; ?></span>
-                </li>
-            </ul>
+<div class="col">
+    <div class="card">
+        <img class="card-img-top" src="<?php echo $album['poster']; ?>" alt="" />
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $album['title']; ?></h5>
+            <div class="card-text">
+                <ul>
+                    <li>
+                        <span class="author_span"><?php echo $album['author']; ?></span>
+                    </li>
+                    <li>
+                        <span class="year_span"><?php echo $album['year']; ?></span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
