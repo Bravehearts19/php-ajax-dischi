95e05e2b17dad44751da691b1df72603d70171c1
Bravehearts19 <leo18111998@gmail.com>
2021-12-01 17:07:49 +0100

    Added select component

--- components/albumCardContainer.php
+++ components/albumCardContainer.php
@@ -1,5 +1,4 @@
 <div class="row_container">
-    <!-- <Select @onSelectedGenre="onSelectedGenre" @onSelectedAuthor="onSelectedAuthor" :genresList="genresList" :authorsList="authorsList"/> -->
     <div class="row row-cols-1 row-cols-md-5 g-5 my-5">
         <?php
         require __DIR__ . "/../data/albums.php";
--- mainVue.js
+++ mainVue.js
@@ -1,9 +1,41 @@
+Vue.component('component-1', {
+    template:   `<div class="d-flex mt-4 gx-3 px-5">
+                    <select class="form-select" aria-label="Default select example" @click="selectGenreOption" v-model="selectGenre">
+                        <option selected value=""></option>
+                        <option :value="genre" v-for="(genre, i) in genresList" :key="i">{{ genre }}</option>
+                    </select>
+
+                    <select class="form-select" aria-label="Default select example" @click="selectAuthorOption" v-model="selectAuthor">
+                        <option selected value=""></option>
+                        <option :value="author" v-for="(author, i) in authorsList" :key="i">{{ author }}</option>
+                    </select>
+                </div>`,
+    props: {
+        genresList: Array,
+        authorsList: Array,
+    },
+    data() {
+        return {
+            selectGenre: "",
+            selectAuthor: "",
+        }
+    },
+    methods: {
+        selectGenreOption() {
+            this.$emit("onSelectedGenre", this.selectGenre)
+        },
+        selectAuthorOption() {
+            this.$emit("onSelectedAuthor", this.selectAuthor)
+        }
+    }
+})
+
 new Vue({
     el: "#app",
     data() {
         return {
             albumsList: [],
-            activeGenreFilter: "Rock",
+            activeGenreFilter: "",
             activeAuthorFilter: "",
         };
     },
